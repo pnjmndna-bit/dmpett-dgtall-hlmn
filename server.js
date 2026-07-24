ca1c0e2a6959a800787cfc42063dfa2253a11f3c
pnjmndna-bit <pnjmndna@gmail.com>
2026-07-25 02:39:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(
 
 app.get("/", (req,res)=>{
     
-    res.send("CRASH... WAKTU BUNDLE HABIS, SERVER MATI UNTUK SEMENTARA");
+    res.send("ERROR... WAKTU BUNDLE HABIS, SERVER MATI UNTUK SEMENTARA");
     
 });
 
